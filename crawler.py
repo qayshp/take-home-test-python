a36101e9c7c37177d5b47ab2a6ecfaffeb4f8c48
Qays H. Poonawala <qayshp@gmail.com>
2015-07-18 17:34:33 -0700

    Store lengths into a map!

--- crawler.py
+++ crawler.py
@@ -3,18 +3,21 @@ import numpy
 import magic
 import sys
 import os
+from collections import defaultdict
+
+lengthMap = defaultdict(int)
 
 def processFile(file, openZip=None):
     if file.lower().endswith(".txt"):
-        print ('Found txt file: {}'.format(file))
+        print('Found txt file: {}'.format(file))
         if (openZip):
             processTxt(openZip.open(file, 'r'))
         else:
             processTxt(open(file))
     elif file.lower().endswith(".zip"):
-        print ('Found zip file: {}'.format(file))
+        print('Found zip file: {}'.format(file))
         processZip(file)
-        print ('Done with zip file: {}'.format(file))
+        print('Done with zip file: {}'.format(file))
 
 def processZip(file):
     with zipfile.ZipFile(file, 'r') as openZip:
@@ -24,7 +27,8 @@ def processZip(file):
 def processTxt(file):
     for line in file:
         for word in line.split():
-            print (word)
+            print(len(word))
+            lengthMap[len(word)] += 1
 
 def processDirectory(dir):
     if (os.path.isdir(dir)):
@@ -35,3 +39,4 @@ def processDirectory(dir):
         print('Error, {} is not a directory.'.format(dir))
 
 processDirectory(os.path.abspath(sys.argv[1]))
+print(str(lengthMap))
