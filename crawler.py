7c4d901db3f3d9f04f940b96cc11ae9cfba42019
Qays H. Poonawala <qayshp@gmail.com>
2015-07-20 00:00:08 -0700

    Change print to log, add histogram plot.

--- crawler.py
+++ crawler.py
@@ -1,23 +1,27 @@
 import zipfile
-import numpy
+import gzip
+import numpy as np
 import magic
 import sys
 import os
+import logging
+import getopt
+import matplotlib.pyplot as plt
 from collections import defaultdict
 
 lengthMap = defaultdict(int)
 
 def processFile(file, openZip=None):
     if file.lower().endswith(".txt"):
-        print('Found txt file: {}'.format(file))
+        logging.info('Found txt file: {}'.format(file))
         if (openZip):
             processTxt(openZip.open(file, 'r'))
         else:
             processTxt(open(file))
     elif file.lower().endswith(".zip"):
-        print('Found zip file: {}'.format(file))
+        logging.info('Found zip file: {}'.format(file))
         processZip(file)
-        print('Done with zip file: {}'.format(file))
+        logging.info('Done with zip file: {}'.format(file))
 
 def processZip(file):
     with zipfile.ZipFile(file, 'r') as openZip:
@@ -27,7 +31,7 @@ def processZip(file):
 def processTxt(file):
     for line in file:
         for word in line.split():
-            print(len(word))
+            logging.debug("%s :%s", len(word), word)
             lengthMap[len(word)] += 1
 
 def processDirectory(dir):
@@ -38,5 +42,20 @@ def processDirectory(dir):
     else:
         print('Error, {} is not a directory.'.format(dir))
 
+log_level = sys.argv[2] if len(sys.argv)>2 else getattr(logging, "INFO")
+logging.basicConfig(level=log_level)
+
 processDirectory(os.path.abspath(sys.argv[1]))
 print(str(lengthMap))
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+x = []
+for key in lengthMap:
+    for i in range(lengthMap[key]):
+        x.append(key)
+numBins = 50
+ax.hist(x,numBins,color='green',alpha=0.8)
+plt.show()
